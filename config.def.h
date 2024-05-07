fc9845410f94317a9e2166092dacc7afa9be2100
Dixit Bhuva <mail@dixit.dev>
2024-05-07 16:05:45 +0530

    Alpha

--- config.def.h
+++ config.def.h
@@ -96,6 +96,9 @@ unsigned int tabspaces = 8;
 
 
 
+/* bg opacity */
+float alpha = 0.9;
+
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
